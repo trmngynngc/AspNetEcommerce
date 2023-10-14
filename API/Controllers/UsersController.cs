a815d97b6e94a870be5a930bf9810914dfc581a3
trmngynngc <trmngynngc@gmail.com>
2023-10-14 10:53:31 +0700

    restore UsersController.cs

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Application.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class UsersController : BaseApiController
+{
+    [HttpPut]
+    public async Task<IActionResult> UpdateBio([FromBody] string bio)
+    {
+        return HandleResult(await Mediator.Send(new EditBio.Command { Bio = bio }));
+    }
+}
