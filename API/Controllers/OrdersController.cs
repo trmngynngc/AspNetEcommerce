663c07163519690b781a44d4a4162fecdc2b1f4e
Nguyen-Tram <tram.nguyenthi.cit19@eiu.edu.vn>
2023-12-07 17:41:54 +0700

    TASK1-10_fix: Orders Controller

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Core;
+using Application.Orders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class OrdersController : ApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(CreateOrderRequestDTO order)
+    {
+        return HandleResult(await Mediator.Send(new Create.Command { OrderDto = order }));
+
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditOrder(Guid id, EditOrderRequestDTO order)
+    {
+        return HandleResult(await Mediator.Send(new Edit.Command { Id = id, Order = order }));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetOrders([FromQuery] PagingParams pagingParams)
+    {
+        return HandleResult(await Mediator.Send(new List.Query { QueryParams = pagingParams }));
+    }
+}
