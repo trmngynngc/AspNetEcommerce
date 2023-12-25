8e6b7c326f6ab7655146a33c34b21c853a832fec
Nguyen-Tram <trmngynngc>
2023-12-25 09:03:08 +0700

    feat(Cart): remove create and delete cart from management

--- API/Controllers/Management/CartsController.cs
+++ API/Controllers/Management/CartsController.cs
@@ -1,19 +1,5 @@
-﻿using Application.Cart.CartDetails;
-using Microsoft.AspNetCore.Mvc;
-
-namespace API.Controllers.Management;
+﻿namespace API.Controllers.Management;
 
 public class CartsController : ManagementApiController
 {
-    [HttpPost("Details")]
-    public async Task<IActionResult> CreateCartDetail(CreateCartDetailRequestDTO cartDetail)
-    {
-        return HandleResult(await Mediator.Send(new Create.Command { CartDetails = cartDetail }));
-    }
-
-    [HttpDelete("Details")]
-    public async Task<IActionResult> DeleteCartDetail(string cartId, Guid productId)
-    {
-        return HandleResult(await Mediator.Send((new Delete.Command { CartId = cartId, ProductId = productId})));
-    }
 }
